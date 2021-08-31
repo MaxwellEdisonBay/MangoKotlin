8e1cd32e7c2c79bbadf3a0108a370bd133751274
Ivan <ivanteslenkopraha@gmail.com>
2021-08-31 23:47:28 +0200

    Firebase authentication

--- app/src/main/java/com/mangoapp/LoginActivity.kt
+++ app/src/main/java/com/mangoapp/LoginActivity.kt
@@ -2,19 +2,46 @@ package com.mangoapp
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val back_btn = findViewById<FloatingActionButton>(R.id.login_back_btn)
+        val loginBtn = findViewById<Button>(R.id.send_btn_login)
+        val backBtn = findViewById<FloatingActionButton>(R.id.login_back_btn)
 
-        back_btn.setOnClickListener {
+        loginBtn.setOnClickListener {
+            performLogin()
+
+        }
+
+        backBtn.setOnClickListener {
             finish()
         }
 
+    }
+
+    private fun performLogin() {
+        val email = findViewById<EditText>(R.id.email_field_login).text.toString()
+        val password = findViewById<EditText>(R.id.password_field_login).text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Fill in all the required fields", Toast.LENGTH_SHORT).show()
+            return
+        }
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+            .addOnFailureListener {
+                Toast.makeText(this, "Failed to sign in", Toast.LENGTH_SHORT).show()
+            }
+            .addOnSuccessListener {
+                Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show()
+            }
     }
 }
--- app/src/main/java/com/mangoapp/MainActivity.kt
+++ app/src/main/java/com/mangoapp/MainActivity.kt
@@ -6,19 +6,20 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val usernameEditText = findViewById<EditText>(R.id.username_field_register)
-        val emailEditText = findViewById<EditText>(R.id.email_field_register)
-        val passwordEditText = findViewById<EditText>(R.id.password_field_register)
 
         val registerBtn = findViewById<Button>(R.id.send_btn_register)
         val loginText = findViewById<TextView>(R.id.login_text)
         registerBtn.setOnClickListener {
-
+            performRegister();
         }
 
         loginText.setOnClickListener {
@@ -26,6 +27,24 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+    }
+
+    private fun performRegister() {
+        val email = findViewById<EditText>(R.id.email_field_register).text.toString()
+        val password = findViewById<EditText>(R.id.password_field_register).text.toString()
+        val username = findViewById<EditText>(R.id.username_field_register).text.toString()
+
+        if (username.isEmpty() || password.isEmpty() || email.isEmpty()) {
+            Toast.makeText(this, "Please, fill in all the required fields", Toast.LENGTH_SHORT).show()
+            return
+        }
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (!it.isSuccessful) return@addOnCompleteListener
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "Failed to create a user", Toast.LENGTH_SHORT).show()
+            }
     }
 
 }

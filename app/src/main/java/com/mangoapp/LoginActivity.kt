0232c5dffe7ed350ff7ea71f5a9632718a32ce47
Ivan <ivanteslenkopraha@gmail.com>
2021-08-31 14:23:57 +0200

    Login activity added; Firebase dependencies were set

--- app/src/main/java/com/mangoapp/LoginActivity.kt
+++ app/src/main/java/com/mangoapp/LoginActivity.kt
@@ -3,11 +3,18 @@ package com.mangoapp
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class LoginActivity: AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val back_btn = findViewById<FloatingActionButton>(R.id.login_back_btn)
+
+        back_btn.setOnClickListener {
+            finish()
+        }
 
     }
 }
